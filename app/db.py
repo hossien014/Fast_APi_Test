0a0978141862a535568ee4f928311921441e2336
hossien <94741775+hossien014@users.noreply.github.com>
2023-10-12 15:42:00 +0330

    add alembic

--- app/db.py
+++ app/db.py
@@ -1,30 +1,30 @@
-# وصل کردن به پوستگرس دیتا بیس 
+# # وصل کردن به پوستگرس دیتا بیس 
 
-from psycopg2 import connect
-from psycopg2._psycopg import cursor
-from os import environ
-from colorama import Style,Fore
-from contextlib import contextmanager
+# from psycopg2 import connect
+# from psycopg2._psycopg import cursor
+# from os import environ
+# from colorama import Style,Fore
+# from contextlib import contextmanager
 
-@contextmanager
-def get_db()->cursor:
-      try:
-            conn = connect(
-                  database="nfast",
-                  user="postgres",
-                  password=environ['password'],
-                  host="127.0.0.1",
-                  port="5432"
-                  )
+# @contextmanager
+# def get_db()->cursor:
+#       try:
+#             conn = connect(
+#                   database="nfast",
+#                   user="postgres",
+#                   password=environ['password'],
+#                   host="127.0.0.1",
+#                   port="5432"
+#                   )
 
-            cursor=conn.cursor()
-            print(Fore.GREEN+"Successfuly connect to database"+Style.RESET_ALL)
-            yield cursor
-      except Exception as exp:      
-            print(Fore.RED+"Connetion to database failed!!"+Style.RESET_ALL)
-      finally:
+#             cursor=conn.cursor()
+#             print(Fore.GREEN+"Successfuly connect to database"+Style.RESET_ALL)
+#             yield cursor
+#       except Exception as exp:      
+#             print(Fore.RED+"Connetion to database failed!!"+Style.RESET_ALL)
+#       finally:
             
-            cursor.close()
-            conn.close()
-            print(Fore.GREEN+"Connecion to database closed"+Style.RESET_ALL)
+#             cursor.close()
+#             conn.close()
+#             print(Fore.GREEN+"Connecion to database closed"+Style.RESET_ALL)
             
